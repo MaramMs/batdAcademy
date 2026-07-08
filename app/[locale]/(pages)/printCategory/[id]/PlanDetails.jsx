01cf1ae48ef760afc1373cf65eb8e3a0b33a4588
Maran.I.Msabeh <maramismail2015@gmail.com>
2026-07-08 16:33:19 +0300

    add show more btn on year plan details page

--- app/[locale]/(pages)/printCategory/[id]/PlanDetails.jsx
+++ app/[locale]/(pages)/printCategory/[id]/PlanDetails.jsx
@@ -2,26 +2,40 @@
 import styleContainer from "@/sass/components/common/container.module.scss";
 import styles from "@/sass/pages/print-category/print-category.module.scss";
 import usePlansStore from "@/store/usePlansStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./Header";
 import Print from "./print";
 import PrintCard from "./PrintCardV2";
 import { useParams, useSearchParams } from "next/navigation";
 import Skeleton from "@/components/ui/Skeleton";
+import { useTranslations } from "next-intl";
+import { ArrowRight } from "lucide-react";
 
 const PrintCategory = () => {
     const searchParams = useSearchParams();
-    const {id} =useParams()
+    const { id } = useParams()
     const search = searchParams.get('search') || '';
-
+    const [visibleCount, setVisibleCount] = useState(4);
     const { handleGetPlanById, plan, isLoading } = usePlansStore();
 
-    const items = plan?.courses?.items || [];
-    useEffect(() => {
-        const query = search ? `?search=${search}` : '';
-        handleGetPlanById(id, query);
-    }, [search,id]);
+    const t = useTranslations('YearPlan')
 
+    console.log(plan, 'plan')
+
+    const items = plan?.courses?.items || [];
+    const handleViewMore = () => {
+        if (plan?.courses && visibleCount < plan.courses.items.length) {
+            setVisibleCount(prev => prev + 6);
+        } else if (plan?.courses?.has_more) {
+            setVisibleCount(prev => prev + 6);
+            const params = new URLSearchParams(window.location.search);
+            params.set('cursor', plan?.courses?.next_cursor);
+            handleGetPlanById(id, `?${params.toString()}`, true);
+        }
+    };
+    useEffect(() => {
+        handleGetPlanById(id);
+    }, []);
 
     return (
         <>
@@ -46,12 +60,18 @@ const PrintCategory = () => {
                             <div className={styleContainer.container}>
                                 <div className={styles.list}>
                                     {
-                                        items?.map((item) => (
+                                        items?.slice(0, visibleCount).map((item) => (
                                             <PrintCard key={item.id} item={item} />
                                         ))
                                     }
 
                                 </div>
+
+                                {(visibleCount < (plan?.courses?.items?.length || 0) || plan?.courses?.has_more) && (
+                                    <button className={styles.showMore} onClick={handleViewMore}>
+                                        {t('viewMore')} <ArrowRight />
+                                    </button>
+                                )}
 
                             </div>
 
